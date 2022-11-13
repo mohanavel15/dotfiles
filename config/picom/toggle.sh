18ac191850dbe3a2d3aec12a662760126c20f681
Mohan <mohanavel15@protonmail.com>
2022-11-13 17:09:53 +0530

    changes & fixes

--- config/picom/toggle.sh
+++ config/picom/toggle.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-if (($(ps -aux | grep [p]icom | wc -l) > 2))
+if (($(ps -aux | grep [p]icom | wc -l) > 0))
 then
   echo Killing Picom
 	polybar-msg hook blur-toggle 1
@@ -8,6 +8,6 @@ then
 else
   echo Starting Picom
 	polybar-msg hook blur-toggle 2
-	picom -b --config=/home/user/.config/picom/picom.conf --experimental-backends &
+	picom -b --config=$HOME/.config/picom/picom.conf --experimental-backends &
 fi
 
